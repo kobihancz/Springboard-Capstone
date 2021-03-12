30b90990f4f5dd750e6abe82d142bab315adfbdb
Kobi Hancz <kobihancz@Kobis-MacBook-Pro-2.local>
2021-03-11 21:02:08 -0800

    collection script

--- data_collection.py
+++ data_collection.py
@@ -1,6 +1,3 @@
 import os
-
-os.system(pip install kaggle)
-os.system(mkdir .kaggle)
-os.system(cp /Users/kobihancz/Downloads/kaggle.json /Users/kobihancz/.kaggle)
-os.system(kaggle datasets download -d worldbank/world-development-indicators)
+os.system('cd .kaggle')
+os.system('kaggle datasets download -d worldbank/world-development-indicators') 
