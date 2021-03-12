185b74e47d357b0d2942b98c1d408046f68f7633
Kobi Hancz <kobihancz@Kobis-MacBook-Pro-2.local>
2021-03-11 20:38:29 -0800

    first commit

---
+++ data_collection.py
@@ -0,0 +1,6 @@
+import os
+
+os.system(pip install kaggle)
+os.system(mkdir .kaggle)
+os.system(cp /Users/kobihancz/Downloads/kaggle.json /Users/kobihancz/.kaggle)
+os.system(kaggle datasets download -d worldbank/world-development-indicators)
