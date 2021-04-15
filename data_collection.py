78263a78283bf2c3133de96e4f88c6d949e6332b
Kobi Hancz <kobihancz@Kobis-MacBook-Pro-2.local>
2021-04-14 21:26:06 -0700

    data collection modified

--- data_collection.py
+++ data_collection.py
@@ -1,3 +1,5 @@
 import os
 os.system('cd .kaggle')
-os.system('kaggle datasets download -d worldbank/world-development-indicators') 
+os.system('kaggle datasets download -d worldbank/world-development-indicators')
+os.system('mv ~/world-development-indicators.zip /Users/kobihancz/Desktop/Springboard_Capstone')
+os.system('unzip world-development-indicators.zip')
