348836b786a5b6459217f917a15de7b290bb2db8
kobihancz <72058053+kobihancz@users.noreply.github.com>
2021-04-23 18:25:51 -0700

    Add files via upload

---
+++ Step3_PrototypingPipeline/Step3_PrototypingPipeline/master_OOP.py
@@ -0,0 +1,21 @@
+from data_collection import DataSets
+from data_cleaning import WorldIndicators
+import pandas as pd
+import zipfile
+
+DATASET = 'worldbank/world-development-indicators'
+
+COUNTRIES = pd.read_csv('/Users/kobihancz/Desktop/Springboard_Capstone/Dataset/Country.csv')
+INDICATORS  = pd.read_csv('/Users/kobihancz/Desktop/Springboard_Capstone/Dataset/Indicators.csv')
+SERIES = pd.read_csv('/Users/kobihancz/Desktop/Springboard_Capstone/Dataset/Series.csv')
+COUNTRY_NOTES = pd.read_csv('/Users/kobihancz/Desktop/Springboard_Capstone/Dataset/CountryNotes.csv')
+SERIES_NOTES = pd.read_csv('/Users/kobihancz/Desktop/Springboard_Capstone/Dataset/SeriesNotes.csv')
+FOOT_NOTES = pd.read_csv('/Users/kobihancz/Desktop/Springboard_Capstone/Dataset/Footnotes.csv')
+
+
+DataSet = DataSets()
+DataSet.dataset_dwnload(DATASET)
+
+word_indicators = WorldIndicators()
+word_indicators.transform_countries()
+word_indicators.transform_indicators()
